21c87c9143de7adfe52360dfcf0dd4deef7edf81
yattadog <kickscar@gmail.com>
2016-11-29 04:12:15 +0900

    예제코드수정

--- app/src/androidTest/java/com/bit2016/loginactivity2/ApplicationTest.java
+++ app/src/androidTest/java/com/bit2016/loginactivity2/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.estsoft.loginactivity2;
+package com.bit2016.loginactivity2;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/bit2016/loginactivity2/MainActivity.java
+++ app/src/main/java/com/bit2016/loginactivity2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.estsoft.loginactivity2;
+package com.bit2016.loginactivity2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        TextView textView = (TextView)findViewById( R.id.tv_signup );
+//        textView.setMovementMethod( LinkMovementMethod.getInstance() );
+//        textView.setText( Html.fromHtml( "<a href=http://www.hellomyoffice.com>Hello My Office 가입하기<;/a>" ));
     }
 }
