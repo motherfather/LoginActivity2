8e8db663da4785a6122557adfb3d49c48cb6bf53
motherfather <father@mail.com>
2016-11-29 13:23:44 +0900

    완료

--- app/src/main/java/com/bit2016/loginactivity2/MainActivity.java
+++ app/src/main/java/com/bit2016/loginactivity2/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView textView = (TextView)findViewById( R.id.tv_signup );
-//        textView.setMovementMethod( LinkMovementMethod.getInstance() );
-//        textView.setText( Html.fromHtml( "<a href=http://www.hellomyoffice.com>Hello My Office 가입하기<;/a>" ));
     }
 }
